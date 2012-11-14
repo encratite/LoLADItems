58cbf74cd7345b2d79f74d33eb03ad0baf0b27cb
void <git.email.address@googlemail.com>
2012-11-14 14:45:22 +0100

    Apparently a lot of the modifications had been left uncommitted for a long time

--- adItems.rb
+++ adItems.rb
@@ -3,33 +3,98 @@ require 'nil/console'
 require_relative 'analyse'
 require_relative 'Item'
 
-doransBlade = Item.new("Doran's Blade", 475, ItemStats.new(attackDamage: 10))
-berserkersGreaves = Item.new("Berserker's Greaves", 920, ItemStats.new(attackSpeed: 0.2))
-bloodthirster = Item.new("Bloodthirster", 3000, ItemStats.new(attackDamage: 100))
-zeal = Item.new("Zeal", 1195, ItemStats.new(attackSpeed: 0.2, criticalStrike: 0.1))
-phantomDancer = Item.new("Phantom Dancer", 2845, ItemStats.new(attackSpeed: 0.55, criticalStrike: 0.3))
-infinityEdge = Item.new("Infinity Edge", 3830, [ItemStats.new(attackDamage: 80, criticalStrike: 0.25), UniqueItemStats.new(criticalStrikeBonus: 0.5)])
-blackCleaver = Item.new("Black Cleaver (1 stack)", 2865, [ItemStats.new(attackDamage: 55, attackSpeed: 0.3), UniqueItemStats.new(flatArmorPenetration: 15)])
-lastWhisper = Item.new("Last Whisper", 2290, [ItemStats.new(attackDamage: 40), UniqueItemStats.new(percentageArmorPenetration: 0.4)])
-witsEnd = Item.new("Wit's End", 2000, UniqueItemStats.new(magicalDamage: 42))
-swordOfTheDivine = Item.new("Sword of the Divine (active)", 1970, [ItemStats.new(attackSpeed: 0.6), UniqueItemStats.new(magicalDamage: 100 / 4, flatArmorPenetration: 30)])
+class ItemCombinator
+  DoransBlade = Item.new("Doran's Blade", 475, ItemStats.new(attackDamage: 10))
+  BerserkersGreaves = Item.new("Berserker's Greaves", 920, ItemStats.new(attackSpeed: 0.2))
+  Bloodthirster = Item.new("Bloodthirster", 3000, ItemStats.new(attackDamage: 100))
+  Zeal = Item.new("Zeal", 1195, ItemStats.new(attackSpeed: 0.2, criticalStrike: 0.1))
+  PhantomDancer = Item.new("Phantom Dancer", 2845, ItemStats.new(attackSpeed: 0.55, criticalStrike: 0.3))
+  InfinityEdge = Item.new("Infinity Edge", 3830, [ItemStats.new(attackDamage: 80, criticalStrike: 0.25), UniqueItemStats.new(criticalStrikeBonus: 0.5)])
+  BlackCleaver = Item.new("Black Cleaver (no stacks)", 2865, ItemStats.new(attackDamage: 55, attackSpeed: 0.3))
+  FullyStackedBlackCleaver = Item.new("Black Cleaver (3 stacks)", 2865, [ItemStats.new(attackDamage: 55, attackSpeed: 0.3), UniqueItemStats.new(flatArmorPenetration: 3 * 15)])
+  LastWhisper = Item.new("Last Whisper", 2290, [ItemStats.new(attackDamage: 40), UniqueItemStats.new(percentageArmorPenetration: 0.4)])
+  WitsEnd = Item.new("Wit's End", 2000, UniqueItemStats.new(magicalDamage: 42))
+  SwordOfTheDivine = Item.new("Sword of the Divine (without active)", 1970, [ItemStats.new(attackSpeed: 0.6), UniqueItemStats.new(magicalDamage: 100 / 4)])
+  SwordOfTheDivineWithActive = Item.new("Sword of the Divine (with active)", 1970, [ItemStats.new(attackSpeed: 0.6), UniqueItemStats.new(magicalDamage: 100 / 4, flatArmorPenetration: 30)])
+  ExecutionersCalling = Item.new("Executioner's Calling", 1350, ItemStats.new(criticalStrike: 0.1))
 
-rows = [[
-  'Description',
-  'Gold',
-  'Single shot damage',
-  'Damage per second',
-]]
+  NonUniqueItems = [Bloodthirster, PhantomDancer]
+  UniqueItems = [InfinityEdge, BlackCleaver, FullyStackedBlackCleaver, LastWhisper, WitsEnd, SwordOfTheDivine, SwordOfTheDivineWithActive, ExecutionersCalling, Zeal]
+  #UniqueItems = [InfinityEdge, LastWhisper]
 
-level = 12
-rows << analyse(level, [doransBlade, berserkersGreaves, bloodthirster])
-rows << analyse(level, [doransBlade, berserkersGreaves, infinityEdge])
+  def self.combine(tankMode, level, limit, rows, uniqueItems = UniqueItems, usedCombinations = [], combination = [])
+    if combination.size == limit
+      stringCheckTargets = [
+        'Black Cleaver',
+        'Sword of the Divine',
+      ]
+      stringCheckTargets.each do |string|
+        hitCounter = 0
+        combination.each do |item|
+          if item.description.index(string) != nil
+            hitCounter += 1
+          end
+        end
+        if hitCounter > 1
+          return
+        end
+      end
+      combinationCheck = combination.sort do |x, y|
+        x.description <=> y.description
+      end
+      if usedCombinations.include?(combinationCheck)
+        return
+      end
+      usedCombinations.push(combinationCheck)
+      items = []
+      if level <= 15
+        items << DoransBlade
+      end
+      if level <= 12
+        items << DoransBlade
+      end
+      items << BerserkersGreaves
+      items += combination
+      row = analyse(tankMode, level, items)
+      rows.push(row)
+    else
+      uniqueItems.each do |item|
+        currentCombination = combination + [item]
+        currentUniqueItems = uniqueItems.dup
+        currentUniqueItems.delete(item)
+        combine(tankMode, level, limit, rows, currentUniqueItems, usedCombinations, currentCombination)
+      end
+      NonUniqueItems.each do |item|
+        currentCombination = combination + [item]
+        combine(tankMode, level, limit, rows, uniqueItems, usedCombinations, currentCombination)
+      end
+    end
+  end
+end
 
-level = 15
-rows << analyse(level, [doransBlade, berserkersGreaves, bloodthirster, phantomDancer])
-rows << analyse(level, [doransBlade, berserkersGreaves, bloodthirster, blackCleaver])
-rows << analyse(level, [doransBlade, berserkersGreaves, infinityEdge, phantomDancer])
-rows << analyse(level, [doransBlade, berserkersGreaves, infinityEdge, blackCleaver])
-rows << analyse(level, [doransBlade, berserkersGreaves, bloodthirster, infinityEdge])
+def processMode(description, tankMode)
+  levels = [12, 15, 18, 18]
 
-Nil.printTable(rows)
+  print "Target: #{description}\n\n"
+
+  itemCount = 1
+  levels.each do |level|
+    print "Number of big AD items at level #{level}: #{itemCount}\n\n"
+    rows = [
+      [
+        'Description',
+        'Gold',
+        'Single shot damage',
+        'Bonus attack damage',
+        'Damage per second',
+      ]
+    ]
+    ItemCombinator.combine(tankMode, level, itemCount, rows)
+    itemCount += 1
+    Nil.printTable(rows)
+    puts ''
+  end
+end
+
+processMode("Caitlyn with flat armor seals and 3/3 armor mastery", false)
+processMode("Gangplank with Wriggle's, Atma's Impaler, flat armor seals and 3/3 armor mastery", true)
--- analyse.rb
+++ analyse.rb
@@ -4,30 +4,48 @@ def damageReductionFactor(resistance)
   return 100.0 / (100.0 + resistance)
 end
 
-def analyse(level, items)
+def analyse(tankMode, level, items)
   #Vayne stats
   baseAttackDamage = 50
   attackDamagePerLevel = 3.25
   baseAttackSpeed = 0.658
   attackSpeedPerLevel = 0.031
 
-  #Caitlyn stats
-  baseArmor = 13
-  armorPerLevel = 3.5
-  baseMagicResistance = 30
-  magicResistancePerLevel = 0
-
-  runeArmor = 13
-  masteryArmor = 3
-
-  itemArmor = 0
+  if !tankMode
+    #Caitlyn stats
+    baseArmor = 13
+    armorPerLevel = 3.5
+    baseMagicResistance = 30
+    magicResistancePerLevel = 0
+    runeArmor = 13
+    masteryArmor = 6
+    itemArmor = 0
+    itemMagicResistance = 0
+  else
+    #Gangplank stats
+    baseArmor = 16.5
+    armorPerLevel = 3.3
+    baseMagicResistance = 30
+    magicResistancePerLevel = 1.25
+    runeArmor = 13
+    masteryArmor = 6
+    itemArmor = 30 + 45
+    itemMagicResistance = 25
+  end
 
   targetArmor = baseArmor + level * armorPerLevel + runeArmor + masteryArmor + itemArmor
-  targetMagicResistance = baseMagicResistance + level * magicResistancePerLevel
+  targetMagicResistance = baseMagicResistance + level * magicResistancePerLevel + itemMagicResistance
 
-  attackDamage = 0
-  attackSpeed = 0.0
-  criticalStrike = 0.0
+  masteryAttackDamage = 3
+  runeAttackDamage = 6.8
+
+  bonusAttackDamage = masteryAttackDamage + runeAttackDamage
+
+  attackDamage = baseAttackDamage + level * attackDamagePerLevel
+  masteryAttackSpeed = 0.06
+  attackSpeed = level * attackSpeedPerLevel + masteryAttackSpeed
+  masteryCriticalStrike = 0.04
+  criticalStrike = masteryCriticalStrike
 
   masteryCriticalStrikeDamage = 0.1
   criticalStrikeDamage = 2.0 + masteryCriticalStrikeDamage
@@ -56,7 +74,7 @@ def analyse(level, items)
       if stats.class == UniqueItemStats
         uniqueStats << stats
       end
-      attackDamage += stats.attackDamage
+      bonusAttackDamage += stats.attackDamage
       attackSpeed += stats.attackSpeed
       criticalStrike += stats.criticalStrike
       criticalStrikeDamage += stats.criticalStrikeBonus
@@ -67,18 +85,10 @@ def analyse(level, items)
     descriptions << item.description
   end
 
+  attackDamage += bonusAttackDamage
+
   description = descriptions.join(', ')
 
-  masteryAttackDamage = 3
-  runeAttackDamage = 6.8
-  attackDamage = baseAttackDamage + level * attackDamagePerLevel + masteryAttackDamage + runeAttackDamage
-
-  masteryAttackSpeed = 0.06
-  itemAttackSpeed = 0.2
-  attackSpeed += level * attackSpeedPerLevel + masteryAttackSpeed + itemAttackSpeed
-
-  masteryCriticalStrike = 0.04
-  criticalStrike += masteryCriticalStrike
   criticalStrike = [criticalStrike, 1.0].min
 
   effectiveArmor = [targetArmor * (1 - armorPenetrationPercentage) * (1 - masteryArmorPenetrationPercentage) - armorPenetration, 0].max
@@ -95,7 +105,8 @@ def analyse(level, items)
     description,
     gold.to_s,
     singleShotDamage.to_s,
-    sprintf('%.1f%%', damagePerSecond),
+    bonusAttackDamage.to_i.to_s,
+    sprintf('%.1f', damagePerSecond),
   ]
   return row
 end
